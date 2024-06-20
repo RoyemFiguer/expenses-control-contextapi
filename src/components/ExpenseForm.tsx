349ae44b14ebb1f2fb93f01bea164626583a4990
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-06-20 16:14:30 -0600

    Added dynamic error component

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css'
 import 'react-date-picker/dist/DatePicker.css'
+import ErrorMessage from "./ErrorMessage";
 
 export default function ExpenseForm() {
     const[expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +13,7 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     })
+    const [error,setError] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) =>{
         const {name, value } = e.target
@@ -29,11 +31,23 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if(Object.values(expense).includes('')){
+            setError('Todos los campos son obligatorios')
+            return
+        }
+
+    }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
         <legend
             className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
         >Nuevo Gasto</legend>
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
 
         <div className="flex flex-col gap-2">
             <label 
