a293891d85cabaa243f2642f26c35d7235cd5ab1
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-06-24 23:05:01 -0600

    Format Date Function Added
    Also more details added to the component

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -6,8 +8,21 @@ type ExpenseDetailProps = {
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
   return (
-    <div>
-      Expense detail
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div>
+
+      </div>
+
+      <div>
+        <p>{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+      </div>
+
+      <AmountDisplay
+        amount={expense.amount}
+      />
+
+
     </div>
   )
 }
